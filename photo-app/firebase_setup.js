288434aa894046ec600139df7ffd8b465a730218
alexchp <74794236+lex-HP@users.noreply.github.com>
2023-05-07 23:56:03 -0400

    Update firebase_setup.js

--- photo-app/firebase_setup.js
+++ photo-app/firebase_setup.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC4h44YG9eH2CQN1oh1XnIpkmB5r9DRUE0",
-  authDomain: "seniordesignfirebase.firebaseapp.com",
-  databaseURL: "https://seniordesignfirebase-default-rtdb.firebaseio.com",
-  projectId: "seniordesignfirebase",
-  storageBucket: "seniordesignfirebase.appspot.com",
-  messagingSenderId: "908816564276",
-  appId: "1:908816564276:web:4c00cfe1e4ebbcd74c0a61",
-  measurementId: "G-F3JNYLYYZB"
+  apiKey: "nice try",
 };
 
 // Initialize Firebase
